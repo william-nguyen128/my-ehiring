8f5f942f2ef0595218d367e5868e0e6bef3236e1
william-nguyen128 <84336299+william-nguyen128@users.noreply.github.com>
2022-08-10 14:55:17 +0700

    build(Frontend - Components): Add Talent component & service

---
+++ frontend/src/app/components/talent/talent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-talent',
+  templateUrl: './talent.component.html',
+  styleUrls: ['./talent.component.less']
+})
+export class TalentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
