c8155656e8f0d15e57456d35881c287854306416
william-nguyen128 <84336299+william-nguyen128@users.noreply.github.com>
2022-08-10 15:17:36 +0700

    build(Frontend - Root): Add HttpClientModule

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { TalentComponent } from './components/talent/talent.component';
 
 @NgModule({
   declarations: [AppComponent, TalentComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
